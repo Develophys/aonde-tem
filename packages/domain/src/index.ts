16acafeeb1ed2d1d5a7292670ef1ee2fb2b96208
Mauricio AB <dev.mauricioalezandre@gmail.com>
2026-06-28 12:14:32 -0300

    feat(domain): export all entities, VOs, and repository ports (AT-013)

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,7 +1,26 @@
-// Public surface of the domain package.
+// Errors
 export * from "./errors/domain-error.js";
+
+// Value Objects
 export * from "./value-objects/coordinates.js";
+export * from "./value-objects/email.js";
+export * from "./value-objects/price.js";
+
+// Entities
 export * from "./entities/place.js";
+export * from "./entities/user.js";
+export * from "./entities/product.js";
+export * from "./entities/discovery.js";
+export * from "./entities/flag.js";
+export * from "./entities/blocked-term.js";
+
+// Repositories (interfaces / ports)
 export * from "./repositories/place-repository.js";
+export * from "./repositories/user-repository.js";
+export * from "./repositories/product-repository.js";
+export * from "./repositories/discovery-repository.js";
+export * from "./repositories/flag-repository.js";
+
+// Ports
 export * from "./ports/logger.js";
 export * from "./ports/geocoding.js";
